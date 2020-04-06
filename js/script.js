93f92db5d1f794e2da23270656d38d0f6cb3b01b
roundraw <nulhh@yandex.ru>
2020-04-07 01:46:09 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -39,6 +39,7 @@ function get(elem) {
 function getRadius(x, y, x2, y2) {
   return Math.sqrt(Math.pow(x - x2, 2) + Math.pow(y - y2, 2))
 };
+
 var ls = localStorage;
 ls.brush = 5;
   function setStageControls() {
