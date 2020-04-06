7964b24f3732af90e0b3e29dc5537513972cedb3
roundraw <nulhh@yandex.ru>
2020-04-07 01:35:27 +0300

    .....

--- js/script.js
+++ js/script.js
@@ -22,9 +22,7 @@ function get(elem) {
 function getRadius(x, y, x2, y2) {
   return Math.sqrt(Math.pow(x - x2, 2) + Math.pow(y - y2, 2))
 };
-get('toolbar').onselectstart = function() {
-  return false
-};
+
 var ls = localStorage;
 ls.brush = "pencil";
 var add = !1;
